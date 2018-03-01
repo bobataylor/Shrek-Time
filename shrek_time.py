7f5ed6755abe55aeaa45fe7a02bdca056b8c16c1
bobataylor <cam@rtastudio.com>
2018-03-01 12:01:28 -0500

    Added method headers for python

---
+++ shrek_time.py
@@ -0,0 +1,21 @@
+import time
+
+EPOCH = 0 #TODO time since april 22 2001
+shrek = 0 #main unit of time
+donkey = 0 #subdivision of a shrek
+
+#returns time in seconds since the EPOCH
+def time():
+
+#returns the local time
+def localtime():
+
+#suspends execution for a defined period of time
+def sleep(wait):
+
+#parses its input into and returns a time_struct
+def strptime():
+
+#returns a struct of the current date/time
+def struct_time();
+
